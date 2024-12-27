2f94f2d609ba15a55a5978dbf576995c332d3748
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2024-12-27 15:59:05 +0000

    Properties

--- screen_sound/Musica.cs
+++ screen_sound/Musica.cs
@@ -6,13 +6,14 @@ class Musica
     public string nome;
     public string artista;
     public int duracao;
-    public bool disponivel;
+    public bool Disponivel { get; set; }
 
+   
     public void ExibirFichaTecnica(){
         Console.WriteLine($"Nome: {nome}");
         Console.WriteLine($"Artista: {artista}");
         Console.WriteLine($"Duração: {duracao}");
-    if(disponivel) {
+    if(Disponivel) {
         Console.WriteLine("disponível no plano");
     } else
     {
