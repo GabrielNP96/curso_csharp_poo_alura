b31e02e3ddcb454c6ee2b65ed36573331edf614e
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2024-12-28 19:17:49 +0000

    Alterando o GET com lambda

--- screen_sound/Musica.cs
+++ screen_sound/Musica.cs
@@ -7,13 +7,14 @@ class Musica
     public string Artista { get; set;}
     public int Duracao { get; set;}
     public bool Disponivel { get; set; }
-    public string NomeCompleto { get; set; }
+    public string DescricaoResumida => $"A música {Nome} pertece {Artista}";
 
    
     public void ExibirFichaTecnica(){
         Console.WriteLine($"Nome: {Nome}");
         Console.WriteLine($"Artista: {Artista}");
         Console.WriteLine($"Duração: {Duracao}");
+        Console.WriteLine($"Descrição: {DescricaoResumida}");
     if(Disponivel) {
         Console.WriteLine("disponível no plano");
     } else
