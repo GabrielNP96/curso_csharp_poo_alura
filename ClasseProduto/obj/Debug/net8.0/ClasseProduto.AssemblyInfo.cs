ac776498c5742f3085efab9b135dfe3aa6401a2f
GabrielNP96 <gabrielnp1996@gmail.com>
2024-12-30 23:43:59 -0300

    Integrando classes e definindo relacionamentos.

--- ClasseProduto/obj/Debug/net8.0/ClasseProduto.AssemblyInfo.cs
+++ ClasseProduto/obj/Debug/net8.0/ClasseProduto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClasseProduto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6c02bbe14cb2b09b9a5c48b8febe61ff5b8a57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69243702de9b6a80fb2f38be87b6d82594ab382d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClasseProduto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClasseProduto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
